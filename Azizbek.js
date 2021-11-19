8fe875b6c415562f359f631befd1234d453fcd89
sanjarbek <88580441+sanjarDev1@users.noreply.github.com>
2021-11-19 23:36:54 +0500

    Add files via upload

---
+++ Azizbek.js
@@ -0,0 +1,33 @@
+
+
+// obj = (num1 = 0, num2 = 0) => {
+//   if (num2) console.log(num1 + num2);
+
+//   return function (num3 = 0) {
+//     console.log(num1 + num3);
+//   };
+// };
+
+// obj(1, 2);
+// obj(1)(2);
+
+
+// const obj1 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   getA() {
+//     console.log(this.a);
+//     return this;
+//   },
+//   getB() {
+//       console.log(this.b);
+//       return this
+//     },
+//     getC() {
+//         console.log(this.c);
+//         return this
+//   }
+// };
+
+// obj1.getA().getB().getC();
