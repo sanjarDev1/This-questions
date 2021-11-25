8ea7d89e89d04bc7db8ccbe8619264c3875e8159
sanjarbek <88580441+sanjarDev1@users.noreply.github.com>
2021-11-25 21:57:13 +0500

    Add files via upload

---
+++ removeDublicate.js
@@ -0,0 +1,9 @@
+const sum2 = (str) => {
+    let yigindi=0;
+    str.split("").forEach(i => {
+    if(Number(i)) yigindi+=Number(i)
+   })
+   console.log(yigindi)
+   }
+   
+   sum2("agi2pyesb94dx2y90ke2wg590n")
