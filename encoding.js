8307c1c88aa85a62be405e937e18c342bf12983f
sanjarbek <88580441+sanjarDev1@users.noreply.github.com>
2021-11-26 21:52:38 +0500

    Add files via upload

---
+++ encoding.js
@@ -0,0 +1,44 @@
+
+
+// var data = {id:1,name:"Asad",status:"students"};
+// const counts = (obj,text)=>{
+// var str =[];
+// var count = 0;
+// Object.values(obj).map((value)=>str+=value)
+// str = str.toLowerCase();
+// text = text.toLowerCase();
+// for(let i =0; i<str.length; i++){
+//     if(str[i]===text){
+//         count++
+//     }
+// }
+// console.log(count);
+// }
+
+// counts(data,"a")
+
+
+// data.map((value)=>{
+//     var a = Object.values(value)
+//     console.log(a);
+
+// ENCODING
+
+const copmress=(text) =>{
+    let count  =0;
+    let result = "";
+    for(let i=0;i<text.length;i++){
+        count++;
+        let current = text[i];
+        if(current!==text[i+1]){
+            if(count==1) count="";
+        result+= count+current;
+        count=0;
+        }
+    }
+return result;
+
+}
+var ar ="AAABBADDTY";
+console.log("AAABBDDTY");
+console.log(copmress(ar));
